9a4f27733cfe5e2af6b376e469edef92bb13e242
wnsgudchl0302 <wnsgudchl0302@naver.com>
2020-09-18 16:20:49 +0900

    Edit App.js , Readme.md 9월 18일 실습내용

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
+function Food(props){
+  console.log(props)
+  return(
+    <h3>I love { props.fav}</h3>
+  );
+}
+
 function App() {
-  return <div className="App" />;
+  // return <div className="App" />;
+  return (
+    <div>
+     <h1>Hello!!!!!</h1>
+     <Food fav="김치" />
+     <Food fav="치킨" />
+     <Food fav="햄버거" />
+     <Food fav="피자" />
+     <Food fav="콜라" />
+    </div>
+     );
 }
 
 export default App;
